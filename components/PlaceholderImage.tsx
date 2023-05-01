46e42e830a9278d15967f83fda2872e79d91147f
Jorge Zreik <jfzreik@gmail.com>
2023-05-01 19:50:32 -0400

    Configure Next.js for testing

--- components/PlaceholderImage.tsx
+++ components/PlaceholderImage.tsx
@@ -4,7 +4,7 @@ export const PlaceholderImage = () => {
   return (
     <div className="placeholder-image-0">
       <img
-        src={vivid_597507b0cf4aa2d50814e6c027ae44c0da1e7c7a}
+        src={vivid_597507b0cf4aa2d50814e6c027ae44c0da1e7c7a.src}
         className="placeholder-image-1"
       />
     </div>
