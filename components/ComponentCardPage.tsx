a7a82704639e88fb8c4dd868304396eec0367213
jorgezreik <jfzreik@gmail.com>
2023-05-09 14:19:26 -0400

    Delete components directory

--- components/ComponentCardPage.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentCard } from "./ComponentCard";
-
-export const ComponentCardPage = () => {
-  return (
-    <ComponentCard draggable={true} text="Card" colors="Zinc" size="Small" />
-  );
-};
