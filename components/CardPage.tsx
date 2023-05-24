b631c64e20734ab8590839acaca50ca46d822290
aryamankha <aryamank@princeton.edu>
2023-05-24 17:45:03 -0400

    clean up

--- components/CardPage.tsx
+++
@@ -1,5 +0,0 @@
-import { Card } from "./Card";
-
-export const CardPage = () => {
-  return <Card />;
-};
--- components/Component_1Page.tsx
+++
@@ -1,5 +0,0 @@
-import { Component_1 } from "./Component_1";
-
-export const Component_1Page = () => {
-  return <Component_1 property_1="Frame 1" />;
-};
