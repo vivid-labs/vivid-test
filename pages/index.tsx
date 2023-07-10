d12631e4ce9406e58f3742e8eabcac2b5cd9289a
Alberto Rigail <albertojrigail@gmail.com>
2023-07-10 10:37:45 -0400

    Remove components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Content } from "../components/Content";
 
 export default function Home() {
   return (
@@ -15,9 +14,7 @@ export default function Home() {
           height: "100vh",
           padding: "1rem",
         }}
-      >
-        <Content />
-      </div>
+      ></div>
     </>
   );
 }
