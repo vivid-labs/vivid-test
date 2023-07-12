a2aba8fdeaa887286be32c511aa0b07de9715791
Alberto Rigail <albertojrigail@gmail.com>
2023-07-12 15:28:02 -0400

    Delete components

--- components/CheckMark.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./CheckMark.module.css";
-import group from "./group.svg";
-export const CheckMark = ({ override }: { override?: any }) => {
-  return (
-    <div style={override} className={styles.checkMark} figma-id="29:532">
-      <img src={group.src} className={styles.group} figma-id="12:97" />
-    </div>
-  );
-};
